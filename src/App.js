97854c3c4f45140ec6c57fcc59310babaca8f534
jannyr08 <pokada816@gmail.com>
2021-03-28 19:13:29 -0400

    restrict to oswego.edu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, {Component} from 'react'
 import GoogleLogin from 'react-google-login'
+import GoogleLogout, {logout} from 'react-google-login'
 import logo from './logo.png'
 
 export class App extends Component {
 
-  responseGoogle=(response)=> {
+  successResponse=(response)=> {
     console.log(response);
     console.log(response.profileObj);
+    // console.log(response.email_verifiedObj);
+  }
 
+  failureResponse =(response) => {
+    console.log("Failed!!!")
   }
   
   render() {
     return (
-      
       <div>
         <img src={logo} className = "center"/>
         <GoogleLogin className ="login-button"
           clientId="644041850309-32m3qpk5jlq07pmqem0tasjph8ge77pp.apps.googleusercontent.com"
           buttonText="Login"
-          onSuccess={this.responseGoogle}
-          onFailuer = {this.responseGoogle}
+          onSuccess={this.successResponse}
+          onFailure = {this.failureResponse}
           cookiePolicy={'single_host_origin'}
+          hostedDomain="oswego.edu"
         />
+
+        {/* <GoogleLogout
+          clientId="644041850309-32m3qpk5jlq07pmqem0tasjph8ge77pp.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={logout}
+          // onLogoutSuccess={logout}
+        /> */}
+
       </div>
     )
   }
